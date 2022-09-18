d06edd52f99bcb22d7eb0bc93153b56d5379eabe
Guilherme Almada <guido.ptr13@gmail.com>
2022-09-17 23:53:06 -0300

    env(nest): nest project initialized

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { TransformInterceptor } from './utils/interceptors/transform.interceptor';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalInterceptors(new TransformInterceptor());
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(3000);
+}
+bootstrap();
