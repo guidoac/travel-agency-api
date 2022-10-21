ceafd1f9dcfb3b86c19d188cce396ae938c7bef6
Guilherme Almada <guido.ptr13@gmail.com>
2022-10-20 22:39:26 -0300

    feat(interceptors): added ClassSerializerInterceptor

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
-import { ValidationPipe } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ResponseTransformInterceptor } from './app/common/interceptors/response-transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalInterceptors(new ResponseTransformInterceptor());
+  app.useGlobalInterceptors(
+    new ResponseTransformInterceptor(),
+    new ClassSerializerInterceptor(app.get(Reflector)),
+  );
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
