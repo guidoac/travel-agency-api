3d9b92a5fa542f8dcf4131cf9763b6a6f2ead7ba
Guilherme Almada <guido.ptr13@gmail.com>
2022-10-22 13:50:53 -0300

    feat(common/jwt): added jwt to common controller

--- src/app/common/controllers/common.controller.ts
+++ src/app/common/controllers/common.controller.ts
@@ -2,12 +2,15 @@ import {
   Controller,
   Post,
   UploadedFile,
+  UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
 import { ImageFileType } from 'src/app/common/types/files';
+import { TheAuthGuard } from '../guards/auth.guard';
 import { TheFileInterceptor } from '../interceptors/file-upload.interceptor';
 import { CommonService } from '../providers/common.service';
 
+@UseGuards(TheAuthGuard)
 @Controller()
 export class CommonController {
   constructor(private commonService: CommonService) {}
